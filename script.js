aa4223475161530b48103fe6783a2797c28a0314
Sasha <beschastnovaleks@gmail.com>
2019-03-14 19:26:37 +0300

    Это мой второй коммит

--- script.js
+++ script.js
@@ -2,18 +2,34 @@
 
 let money = prompt("Ваш бюджет за месяц?", ""),
     time = prompt("Введите дату в формате YYYY-MM-DD", "");
-var a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    b = prompt("Во сколько обойдется?", "");
 let appdata = {
     budget: money,
     timedata: time,
-    expenses: {
-        a: b
-    },
+    expenses: {},
     optionalExpenses: {
 
     },
     income: [],
     savings: false
 };
-alert(money / 30);
+for (let i = 0; i < 2; i++) {
+    var a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        b = prompt("Во сколько обойдется?", "");
+    if (typeof (a) === 'string' && (typeof (a) != null && typeof (b) != null) && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appdata.expenses[a] = b;
+    } else {
+
+    }
+};
+appdata.moneyPerDay = appdata.budget / 30;
+
+alert("Ежедневный бюджет: " + appdata.moneyPerDay);
+if (appdata.moneyPerDay < 100) {
+    console.log("min level money");
+
+} else if (appdata.moneyPerDay > 100 && appdata.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appdata.moneyPerDay > 2000) {
+    console.log(" high level money");
+} else console.log("Error");
